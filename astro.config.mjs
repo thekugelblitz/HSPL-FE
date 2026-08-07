880f28363e4d642075a17daf000210d618a6a1b3
thekugelblitz <dj.dhruval@gmail.com>
2026-08-07 19:46:01 +0530

    feat: implement multiple new hosting product pages, update configuration, and add application base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
@@ -9,6 +7,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });
